e361cf679c3811be0fbd6833744939a31703378b
YouRanju <dbfkswn0321@naver.com>
2019-05-28 15:51:39 +0900

    #2 ListView 항목을 추가하고 보는 코드 구현

--- ListView/Form1.Designer.cs
+++ ListView/Form1.Designer.cs
@@ -43,6 +43,7 @@
             // 
             // lvView
             // 
+            this.lvView.BackColor = System.Drawing.SystemColors.Window;
             this.lvView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.chName,
             this.chAge,
@@ -55,6 +56,7 @@
             this.lvView.TabIndex = 0;
             this.lvView.UseCompatibleStateImageBehavior = false;
             this.lvView.View = System.Windows.Forms.View.Details;
+            this.lvView.Click += new System.EventHandler(this.LvView_Click);
             // 
             // lblName
             // 
@@ -112,6 +114,7 @@
             this.btnAdd.TabIndex = 7;
             this.btnAdd.Text = "추가";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
             // 
             // chName
             // 
--- ListView/Form1.cs
+++ ListView/Form1.cs
@@ -11,6 +11,67 @@ namespace ListView
 {
     public partial class Form1 : Form
     {
+
+        string strName, strAge, strWork;
+
+        private bool TextCheck()
+        {
+            if (this.txtName.Text == "")
+            {
+                MessageBox.Show("이름을 입력하세요.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.txtName.Focus();
+                return false;  
+            }
+            if (this.txtAge.Text == "")
+            {
+                MessageBox.Show("나이을 입력하세요.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.txtAge.Focus();
+                return false;
+            }
+            if (this.txtWork.Text == "")
+            {
+                MessageBox.Show("직업을 입력하세요.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.txtWork.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void LvView_Click(object sender, EventArgs e)
+        {
+            ListViewItem lvi = lvView.SelectedItems[0];
+            string name = lvi.SubItems[0].Text;
+            string age = lvi.SubItems[1].Text;
+            string work = lvi.SubItems[2].Text;
+
+            MessageBox.Show("이름 : " + name 
+                + "\n나이: " + age
+                + "\n직업: " + work,
+                "정보", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            if(TextCheck())
+            {
+                this.strName = this.txtName.Text;
+                this.strAge = this.txtAge.Text;
+                this.strWork = this.txtWork.Text;
+
+                ListViewItem lvi = new ListViewItem(
+                    new string[] { this.strName, this.strAge, this.strWork });
+
+                this.lvView.Items.Add(lvi);
+                this.txtName.Text = "";
+                this.txtAge.Text = "";
+                this.txtWork.Text = "";
+                this.txtName.Focus();
+            }
+
+            
+        }
+
         public Form1()
         {
             InitializeComponent();
